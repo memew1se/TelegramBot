bd5cba97213760471965d7a457299da0fefd308f
Mark <63961428+memew1se@users.noreply.github.com>
2020-08-08 11:43:25 +0300

    Updated

--- handlers.py
+++ handlers.py
@@ -7,6 +7,7 @@ async def send_to_admin(*args):
     await bot.send_message(chat_id = admin_id, text = 'Бот запущен (ый)')
 
 
+@dp.message_handler()
 async def echo(message: Message):
     text = f"Привет, ты написал {message.text}"
     await message.reply(text=text)
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ dp = Dispatcher(bot)
 
 
 if __name__ == '__main__':
-    from handlers import send_to_admin
+    from handlers import send_to_admin, dp
     executor.start_polling(dp, on_startup=send_to_admin)

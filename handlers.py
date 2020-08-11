812937c1127f676314618dad35c418838365294b
Mark <63961428+memew1se@users.noreply.github.com>
2020-08-11 13:17:11 +0300

    Change reply to answer

--- handlers.py
+++ handlers.py
@@ -10,5 +10,5 @@ async def send_to_admin(*args):
 @dp.message_handler()
 async def echo(message: Message):
     text = f"Привет, ты написал {message.text}"
-    await message.reply(text=text)
+    await message.answer(text=text)
 

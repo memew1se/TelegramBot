1cf0cdd7ef6efdeb0ff7ff9098aec62664845a84
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-02 23:57:52 +0300

    Change structure

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from aiogram import Bot, Dispatcher, executor
-from config import BOT_TOKEN
+from data.config import BOT_TOKEN, ADMIN_ID
 
 
 bot = Bot(BOT_TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot)
 
 
+async def send_to_admin(*args):
+    await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен (ый)')
+
 if __name__ == '__main__':
-    from handlers import send_to_admin, dp
+    from handlers import dp
     executor.start_polling(dp, on_startup=send_to_admin)
--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,10 +1,5 @@
-from main import bot, dp
+from loader import dp
 from aiogram.types import Message
-from config import ADMIN_ID
-
-
-async def send_to_admin(*args):
-    await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен (ый)')
 
 
 @dp.message_handler()

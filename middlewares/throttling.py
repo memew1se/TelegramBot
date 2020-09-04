fdbff339280472bf1a15231888744d3d4fac7549
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-04 23:42:52 +0300

    Add middleware based on aiogram's examples

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -1,16 +1,19 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.dispatcher import DEFAULT_RATE_LIMIT
 from aiogram.dispatcher.handler import CancelHandler, current_handler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.utils.exceptions import Throttled
 
-TOKEN = '1208332440:AAFz6AsDiTBea1ViY7s6312ZtkBHWwEtu6s'
+TOKEN = 'BOT_TOKEN_HERE'
 
+# In this example Redis storage is used
+storage = RedisStorage2(db=5)
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
 
 def rate_limit(limit: int, key=None):
@@ -100,18 +103,3 @@ class ThrottlingMiddleware(BaseMiddleware):
         # If current message is not last with current key - do not send message
         if thr.exceeded_count == throttled.exceeded_count:
             await message.reply('Unlocked.')
-
-
-@dp.message_handler(commands=['start'])
-@rate_limit(5, 'start')  # this is not required but you can configure throttling manager for current handler using it
-async def cmd_test(message: types.Message):
-    # You can use this command every 5 seconds
-    await message.reply('Test passed! You can use this command every 5 seconds.')
-
-
-if __name__ == '__main__':
-    # Setup middleware
-    dp.middleware.setup(ThrottlingMiddleware())
-
-    # Start long-polling
-    executor.start_polling(dp)

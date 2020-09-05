d329a6f0bfc0fcffe504e4c6da37d299d0f065e6
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-05 15:53:56 +0300

    Update echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -4,6 +4,5 @@ from aiogram.types import Message
 
 @dp.message_handler()
 async def echo(message: Message):
-    with open("lain.jpg", "rb") as photo:
-        text = f"Привет, ты написал {message.text}"
-        await message.reply_photo(photo, caption=text)
+    text = f"Привет, ты написал {message.text}"
+    await message.answer(text=text)

7366684dad8feebab0e82011a592455dde8b0651
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-09 22:58:24 +0300

    Disable echo.py

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -3,7 +3,7 @@ from aiogram.types import Message
 from middlewares import rate_limit
 
 
-@rate_limit(2)
+# @rate_limit(2)
 # @dp.message_handler()
 async def echo(message: Message):
     text = f"Привет, ты написал {message.text}"

e38e4c52720ee1ec1b50a20e591fa32e22f2850e
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-10 22:38:42 +0300

    Update menu_handlers.py

--- handlers/users/menu_handlers.py
+++ handlers/users/menu_handlers.py
@@ -2,22 +2,30 @@ from aiogram.dispatcher.filters import Command, Text
 from aiogram.types import Message, ReplyKeyboardRemove
 from keyboards.default import menu
 from middlewares import rate_limit
-from loader import dp
+from loader import dp, bot
 
 
-@rate_limit(2)
+@rate_limit(1)
 @dp.message_handler(Command("menu"))
 async def show_menu(message: Message):
     await message.answer("Chose shell ", reply_markup=menu)
 
 
-@rate_limit(2)
+@rate_limit(1)
 @dp.message_handler(Text(equals=["Git", "git"]))
 async def git_shell(message: Message):
     await message.answer(f"Enter commands for Git ", reply_markup=ReplyKeyboardRemove())
 
 
-@rate_limit(2)
+@rate_limit(1)
 @dp.message_handler(Text(equals=["ssh", "SSH"]))
 async def ssh_shell(message: Message):
+    print(message)
+    print(type(message))
     await message.answer(f"Enter commands for SSH ", reply_markup=ReplyKeyboardRemove())
+
+
+@rate_limit(1)
+@dp.message_handler(Text(equals="Never mind..."))
+async def default_shell(message: Message):
+    await message.answer(f"Ok... ", reply_markup=ReplyKeyboardRemove())

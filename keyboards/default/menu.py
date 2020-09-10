012196eb4084ce18cc37d888015e025f45506807
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-10 17:00:59 +0300

    Update menu.py

--- keyboards/default/menu.py
+++ keyboards/default/menu.py
@@ -3,7 +3,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 menu = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text="git")
+            KeyboardButton(text="Git"),
+            KeyboardButton(text="SSH")
         ]
     ],
     resize_keyboard=True

775a2a06d0f86face7b5e7743ff8544e767e467e
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-12 14:16:04 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -8,10 +8,10 @@ storage = MemoryStorage()
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
 
-logging.basicConfig(format=u'%(filename)s '
-                           u'[LINE:%(lineno)d] '
-                           u'#%(levelname)-8s '
-                           u'[%(asctime)s]  '
-                           u'%(message)s',
+logging.basicConfig(format='%(filename)s '
+                           '[LINE:%(lineno)d] '
+                           '#%(levelname)-8s '
+                           '[%(asctime)s]  '
+                           '%(message)s',
                     level=logging.INFO,
                     )

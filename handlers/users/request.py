a825381c8159a117cd2f63166829eb518e5f5ec7
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-28 01:32:10 +0300

    Update request.py

--- handlers/users/request.py
+++ handlers/users/request.py
@@ -1,8 +1,16 @@
 from aiogram.types import Message
+from aiogram.dispatcher import FSMContext
 
+from states.request_states import RequestGroup
 from utils.misc.parcer import parse
 
 from loader import dp, bot
+
+
+@dp.message_handler(state=RequestGroup.R_git)
+async def find_git_command(message: Message, state: FSMContext):
+    await message.answer(parse("git-" + message["text"]))
+    await state.finish()
 
 
 @dp.message_handler()

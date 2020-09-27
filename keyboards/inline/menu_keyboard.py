04ede67841d0f880e413528919efb49a12ffda76
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-28 01:31:36 +0300

    Update menu_keyboard.py

--- keyboards/inline/menu_keyboard.py
+++ keyboards/inline/menu_keyboard.py
@@ -1,17 +1,12 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.callback_data import CallbackData
 
-shell_callback = CallbackData("shell", "shell_name", "command")
+shell_callback = CallbackData("shell", "shell_name")
 
-shell_keyboard = InlineKeyboardMarkup(row_width=2)
+shell_keyboard = InlineKeyboardMarkup(row_width=1)
 
-git_button = InlineKeyboardButton(text="git", callback_data=shell_callback.new(shell_name="git",
-                                                                               command="None"))
+git_button = InlineKeyboardButton(text="git", callback_data=shell_callback.new(shell_name="git"))
 shell_keyboard.insert(git_button)
-
-ssh_button = InlineKeyboardButton(text="ssh", callback_data=shell_callback.new(shell_name="ssh",
-                                                                               command="None"))
-shell_keyboard.insert(ssh_button)
 
 cancel_button = InlineKeyboardButton(text="Cancel", callback_data="cancel")
 shell_keyboard.insert(cancel_button)

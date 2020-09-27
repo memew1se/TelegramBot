b84dc294e88b83a46be1a790080f77639ab44056
Mark <63961428+memew1se@users.noreply.github.com>
2020-09-27 23:57:31 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,6 +2,7 @@ import logging
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 from data import config
 
@@ -10,6 +11,7 @@ storage = MemoryStorage()
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=storage)
 
+dp.middleware.setup(LoggingMiddleware())
 logging.basicConfig(format='%(filename)s '
                            '[LINE:%(lineno)d] '
                            '#%(levelname)-8s '
